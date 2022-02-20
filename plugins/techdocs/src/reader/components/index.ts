ad8b754a5b0b0c28b98358dcddf7aa68b5ca9e72
Camila Belo <camilaibs@gmail.com>
2022-02-20 10:49:02 +0100

    feat(techdocs): create techdocs shadow dom
    Signed-off-by: Camila Belo <camilaibs@gmail.com>

--- plugins/techdocs/src/reader/components/index.ts
+++ plugins/techdocs/src/reader/components/index.ts
@@ -19,6 +19,7 @@ export * from './TechDocsPage';
 export * from './TechDocsPageHeader';
 export * from './TechDocsPageLayout';
 export * from './TechDocsStateIndicator';
+export * from './TechDocsShadowDom';
 
 /**
  * Note: this component is currently being exported so that we can rapidly
